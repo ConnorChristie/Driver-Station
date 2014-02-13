c8037ce1ad70c5b1b8e864801b251ddc5eef4713
Connor <Connor@Connors-MacBook-Pro.local>
2014-02-13 17:29:22 -0600

    Initial Commit

---
+++ Driver Station/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Driver Station
+//
+//  Created by Connor on 2/13/14.
+//  Copyright (c) 2014 Connor Christie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Driver Station/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Driver Station
+//
+//  Created by Connor on 2/13/14.
+//  Copyright (c) 2014 Connor Christie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
